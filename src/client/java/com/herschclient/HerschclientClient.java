c1dec179e5b922fb73326f56282cee0a5ca02dbb
dagsevenonur <onurdagseven01@gmail.com>
2026-02-02 23:59:47 +0300

    first commit

---
+++ src/client/java/com/herschclient/HerschclientClient.java
@@ -0,0 +1,10 @@
+package com.herschclient;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HerschclientClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
